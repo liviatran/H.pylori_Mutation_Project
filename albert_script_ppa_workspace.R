48942610d3d348b21e66178c85367b2ff27e42c6
Albert Wong <albertewong@gmail.com>
2017-12-07 12:32:59 -0800

    reading genbank file as a single string

--- albert_script_ppa_workspace.R
+++ albert_script_ppa_workspace.R
@@ -1,1 +1,9 @@
 library(genbankr)
+
+
+#define file, using a file that has 2 elements
+fileName <-"ppa-sequences-2-results.txt"
+
+#read as string
+singleString <- paste(readLines(fileName), collapse=" ")
+gregexpr("ountry", singleString)
