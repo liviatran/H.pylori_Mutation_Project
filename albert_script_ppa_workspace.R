3e941ac6669ffc5ccd74240af60593ec42e5af95
Albert Wong <albertewong@gmail.com>
2017-12-07 13:07:26 -0800

    update on alberts scripts to make 10 element data frame with accession ids

--- albert_script_ppa_workspace.R
+++ albert_script_ppa_workspace.R
@@ -7,3 +7,16 @@ fileName <-"ppa-sequences-2-results.txt"
 #read as string
 singleString <- paste(readLines(fileName), collapse=" ")
 gregexpr("ountry", singleString)
+View(singleString)
+substring(singleString,3852)
+
+df_ppa<-read.table("ppa-accessions.seq")
+class(df_ppa)
+head(df_ppa)
+colnames(df_ppa)<- "AccessionID"
+
+df_ppa10<-df_ppa[1:10,]
+df_ppa10
+
+data.frame(AccessionID=df_ppa10)->dataf_ppa10
+write.csv(dataf_ppa10, "ppa_df_10_elements.csv")
