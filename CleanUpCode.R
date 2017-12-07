51deefc541aff1ab8fc5453245f3e35d2288b30a
nsallen <nsallen93@gmail.com>
2017-12-07 12:12:21 -0800

    Getting data from GenBank

--- CleanUpCode.R
+++ CleanUpCode.R
@@ -1,1 +1,29 @@
 # Official Code for "Cleaning Up" GenBank Sequence Files
+library(seqinr)
+library(ape)
+library(genbankr)
+mutyseq <- read.fasta(file="mutY-sequences.gb", as.string=TRUE, forceDNAtolower=FALSE)
+mutyseq <- read.GenBank("LC271864.1", species.names =TRUE, as.character=FALSE)
+str(mutyseq)
+
+
+mutyseq <- read.dna(file="mutY-sequences.gb", format = "sequential")
+install.packages("genbankr")
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("genbankr")
+
+mutygb <- import(con = "sequence(1).gb")
+View(sources(mutygb))
+
+mutygb
+str(mutygb)
+
+showMethods("otherFeatures")
+otherFeatures(mutygb)
+
+accession(mutygb)
+definition(mutygb)
+locus(mutygb)
+sources(mutygb)
+
