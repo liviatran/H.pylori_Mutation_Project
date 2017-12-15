ba165e00cf331cbce39f155890e5d5c7771a9e39
liviatran <tran.livia@berkeley.edu>
2017-12-14 21:32:47 -0800

    albert and nikki, look at me

--- liv_working_ppa.R
+++ liv_working_ppa.R
@@ -2,26 +2,68 @@ setwd("~/bioinfofinal")
 
 library(ape)
 library(seqinr)
+ppadf<-read.csv("ppa_df_clean.df")
 
+View(ppaconsensus_matrix)
 
-ppafasta<-read.fasta("ppa-fasta-tree")
+ppafasta <- read.fasta("ppa-fasta-tree")
+ppaaligned <- read.alignment("ppa-fasta-tree", format = "fasta", forceToLower = T)
+ppaconsensus_matrix <- read.dna("ppa-fasta-tree", format="fasta", as.character = TRUE)
 
-ppaaligned<-read.alignment("ppa-fasta-tree", format="fasta")
-ppamatrix<-read.dna("ppa-fasta-tree", format="fasta", as.character=TRUE)
-
-cons<-seqinr::consensus(ppaaligned)
-
-cons
-
-for(i in 1:ppadf$Sequence){
-  if(ppadf$Sequence=!"cons"){
-    mutfreq<-length(which(ppamatrix[,i]==cons[i]))/(nrow(ppamatrix))}
+meanFreq <- function(ppafasta){
+  library(seqinr)
+  numberofsequences <- length(ppafasta)
+  ppaconsensus_length <- length(ppaconsensus)
+  number_column <- seq(1, ppaconsensus_length)
+  ppanewdf <- data.frame("num" = number_column, "MeanFreq" = 0, "wtnt" = ppaconsensus)
+  for(x in 1:ppaconsensus_length){
+    current_base <- ppaconsensus[x]
+    current_matrix_base_count <- ppaconsensus_matrix[,x]
+    ts_count <- 0
+    if(current_base == "a"){
+      ts_count <- current_matrix_base_count[["g"]]
+    }
+    if(current_base == "a"){
+      ts_count <- current_matrix_base_count[["t"]]
+    }
+    if(current_base == "a"){
+      ts_count <- current_matrix_base_count[["c"]]
+    }
+    if(current_base == "g"){
+      ts_count <- current_matrix_base_count[["a"]]
+    }
+    if(current_base == "g"){
+      ts_count <- current_matrix_base_count[["t"]]
+    }
+    if(current_base == "g"){
+      ts_count <- current_matrix_base_count[["c"]]
+    }
+    if(current_base == "c"){
+      ts_count <- current_matrix_base_count[["t"]]
+    }
+    if(current_base == "c"){
+      ts_count <- current_matrix_base_count[["a"]]
+    }
+    if(current_base == "c"){
+      ts_count <- current_matrix_base_count[["g"]]
+    }
+    if(current_base == "t"){
+      ts_count <- current_matrix_base_count[["c"]]
+    }
+    if(current_base == "t"){
+      ts_count <- current_matrix_base_count[["a"]]
+    }
+    if(current_base == "t"){
+      ts_count <- current_matrix_base_count[["g"]]
+    }
+    ppanewdf$meanfreq<- ts_count/number_of_seqs
   }
+  ppanewdf$wtnt<-as.character(ppanewdf$wtnt)
+  return(ppanewdf)
+}
 
-  View(ppamatrix)
+View(ppanewdf)
 
-
-  
 
 
 
