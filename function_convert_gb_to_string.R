ec706984d8b0a2753e7aef7d6b5ebfca056ed370
Albert Wong <albertewong@gmail.com>
2017-12-19 21:54:38 -0800

    delete extra files

--- function_convert_gb_to_string.R
+++
@@ -1,7 +0,0 @@
-#function to read .gb file into a single string
-
-GbtoString<-function(fileName){
-singleString <- paste(readLines(fileName), collapse=" ")
-}
-
-print("To use function, GbtoString('yourfilename') -> 'your variable'")
